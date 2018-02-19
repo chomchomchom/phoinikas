b1092caf096ae03ea7af06b3ae3a412d40b70c57
Chom <wanrudee.phoinikas@gmail.com>
2018-02-19 16:17:51 +0700

    first commit

---
+++ test3.php
@@ -0,0 +1,13 @@
+<?php
+
+/*--------------------------------------------------------------
+*  Author: chom
+*  Date: 2018-02-19
+*  Copyright (c) Your Corporation. All rights reserved.
+*  Licensed under the MIT License.
+*-------------------------------------------------------------*/
+
+print "3";
+
+
+?>
